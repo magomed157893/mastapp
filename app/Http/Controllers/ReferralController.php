4e46bc45c9d21a0b664069bebdd100ec91194cde
Magomed Aliev <magomed157893@gmail.com>
2026-09-24 20:32:05 +0300

    THE END

--- app/Http/Controllers/ReferralController.php
+++ app/Http/Controllers/ReferralController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Referral;
+use App\Models\ReferralEarning;
 use App\Services\Referral\ReferralService;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,7 +19,20 @@ class ReferralController extends Controller
         /** @var \App\Models\Master */
         $master = $request->attributes->get('current_master');
 
-        return response()->json(['master' => $master]);
+        $validated = $request->validate(['code' => 'required|string']);
+
+        $referral = $this->referrals->registerReferral($master, $validated['code']);
+        if (is_null($referral)) {
+            return response()->json(
+                ['message' => 'Refferal cannot be attached'],
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        return response()->json(
+            ['referral' => $referral],
+            $referral->wasRecentlyCreated ? Response::HTTP_CREATED : Response::HTTP_OK
+        );
     }
 
     public function my(Request $request): Response
@@ -25,7 +40,20 @@ class ReferralController extends Controller
         /** @var \App\Models\Master */
         $master = $request->attributes->get('current_master');
 
-        return response()->json([]);
+        $referrals = $master->referrals()
+            ->with(['referredMaster', 'referralEarnings'])
+            ->get();
+
+        return response()->json([
+            'referrals' => $referrals->map(function (Referral $referral) {
+                return [
+                    'name' => $referral->referredMaster->name,
+                    'attached_at' => $referral->created_at,
+                    'rewarded' => $referral->status === Referral::STATUS_REWARDED,
+                    'earned' => $referral->referralEarnings->sum('amount')
+                ];
+            })
+        ]);
     }
 
     public function earnings(Request $request): Response
@@ -33,6 +61,23 @@ class ReferralController extends Controller
         /** @var \App\Models\Master */
         $master = $request->attributes->get('current_master');
 
-        return response()->json([]);
+        $total = $master->referralEarnings()->sum('amount');
+
+        $pending = $master->referralEarnings()
+            ->where('status', ReferralEarning::STATUS_PENDING)
+            ->sum('amount');
+
+        $paid = $master->referralEarnings()
+            ->where('status', ReferralEarning::STATUS_PAID)
+            ->sum('amount');
+
+        $rewardedReferrals = $master->referrals()->active()->count();
+
+        return response()->json([
+            'total' => $total,
+            'pending' => $pending,
+            'paid' => $paid,
+            'rewarded_referrals' => $rewardedReferrals
+        ]);
     }
 }
